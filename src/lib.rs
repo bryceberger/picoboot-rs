857878f51eafdabe03d5703a612003dbf754f5dc
Logan H-D <notquiteapex@gmail.com>
2024-11-27 20:38:00 -0500

    even more minor formatting fixes
    lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! [![github]](https://github.com/NotQuiteApex/picoboot-rs)&ensp;[![crates-io]](https://crates.io/crates/picoboot-rs)&ensp;[![docs-rs]](https://docs.rs/picoboot-rs)
+//! [![github]](https://github.com/NotQuiteApex/picoboot-rs) &ensp; [![crates-io]](https://crates.io/crates/picoboot-rs) &ensp; [![docs-rs]](https://docs.rs/picoboot-rs)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
